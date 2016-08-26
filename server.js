848c679321d0f10eaf03d3566e91d0aebf46e02b
Anthony Kesterton <akesterton@redhat.com>
2016-08-26 22:03:03 +0100

    Created simple server

--- server.js
+++ server.js
@@ -1,4 +1,17 @@
+// Load the http module to create an http server.
+var http = require('http');
 
+// Configure our HTTP server to respond with Hello World to all requests.
+var server = http.createServer(function (request, response) {
+  response.writeHead(200, {"Content-Type": "text/plain"});
+  response.end("Hello OpenShift v3 World\n");
+});
+
+var port = process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 8080,
+    ip   = process.env.IP   || process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0';
+// Listen on port 8000, IP defaults to 127.0.0.1
+
+server.listen(port,ip);
 
 // Debug message
 console.log("Hello, OSE3");
